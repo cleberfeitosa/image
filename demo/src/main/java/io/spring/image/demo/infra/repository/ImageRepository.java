5fe75d543b05e76f165f0025cce90c7ccf95cbbd
Cleber Feitosa <cleber.feitosa@ifmt.edu.br>
2026-06-18 20:27:37 -0400

    Primeira parte Back finalizada

--- demo/src/main/java/io/spring/image/demo/infra/repository/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package io.spring.image.demo.infra.repository;
-
-import io.spring.image.demo.domain.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, String> {
-
-}
